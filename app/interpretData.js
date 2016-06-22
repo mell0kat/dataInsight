192d44dcce82eb15336424d21f2554cf8e256cd7
Katherine Mello <katherine.mello8@gmail.com>
2016-06-22 09:00:17 -0400

    merges data sets

--- app/interpretData.js
+++ app/interpretData.js
@@ -8,7 +8,7 @@ const csv1 = fs.readFileSync("source1.csv", "utf8");
 const csv2 = fs.readFileSync("source2.csv", "utf8");
 
 
-function parseData(csv, lineSeparator) {
+function parseData(csv, lineSeparator, removeDups) {
 	let rows;
 	if (lineSeparator === 'n') {
 		rows = csv.split('\r\n');
@@ -19,12 +19,18 @@ function parseData(csv, lineSeparator) {
 
 	let headers = rows[0].split(','); //type array
 
-	let body = rows.slice(1).map(row => row.split(',')); // type array of arrays
+	let body = rows.slice(1);
+	console.log('body before remove dups:', body.length);
+	if (removeDups) body = removeDups(body);
+	console.log('body after remove dups', body.length);
+
+	body = body.map(row => row.split(',')); // type array of arrays
 
 	//for some reason, the last line is an empty string
-	console.log((body[body.length-1] == ''))
-	if (body[body.length-1] == '') {
-		body = body.slice(0, body.length-1);
+
+	if (body[0] == '') {
+
+		body = body.slice(1);
 	}
 
 
@@ -33,6 +39,7 @@ function parseData(csv, lineSeparator) {
 			let dataObj = {};
 
 			for (var i = 0; i < numColumns; i++) {
+
 				dataObj[headers[i]] = column[i];
 			}
 			return dataObj;
@@ -42,12 +49,14 @@ function parseData(csv, lineSeparator) {
 };
 
 //gets rid of headers
-let test = csv2.split('\r\n').slice(1);
-test = test.sort();
-console.log(test.length)
-console.log(removeDups(test).length)
-function removeDups(array, idx){
+// let test = csv2.split('\r\n').slice(1);
+// test = test.sort();
+// console.log(test.length)
+// console.log(removeDups(test).length)
 
+
+function removeDups(array, idx){
+	array.sort()
 	let noDups = [];
 	for (var i = 0; i< array.length; i++){
 		if (noDups[noDups.length-1] !== array[i]) {
@@ -56,8 +65,62 @@ function removeDups(array, idx){
 	}
 	return noDups;
 }
+
+function grabMonth(dateString) {
+	return dateString.split('-')[1];
+};
+
+function findInMonth(array, month){
+	let elementsInMonth = [];
+
+	for (var i = 0; i< array.length; i++){
+
+			if (grabMonth(array[i].date) === month){
+			elementsInMonth.push(array[i]);
+			}
+
+
+	}
+
+	return elementsInMonth;
+}
+
+function findObjectWithKey(array, key, value){
+	console.log(key, value)
+	return array.filter(item => (item[key] === value)
+		)[0]
+}
+
+//merging obj2 into obj1
+function mergeObjects(obj1, obj2) {
+	let merged = obj1;
+	for (let key in obj2){
+		if (!merged[key]){
+			merged[key] = obj2[key];
+		}
+	};
+	return merged;
+};
+
+function mergeDataSets (set1, set2, joinWith){
+	let merged = [];
+	for(var i = 0; i< set1.length; i++) {
+		let commonObj = findObjectWithKey(set2, joinWith, set1[joinWith]);
+		if (commonObj) {
+			console.log('common obj found:', common obj)
+			merged.push(mergeObjects(set1[i], commonObj))}
+	}
+};
+
 // (parseData(csv1, 'n').length);
-module.exports = parseData;
+let data = parseData(csv2, 'n', removeDups);
+console.log(data)
+
+module.exports = {
+	parseData: parseData,
+	findObjectWithKey: findObjectWithKey,
+	mergeObjects: mergeObjects
+	}
 
 
 
--- test/interpretData.js
+++ test/interpretData.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const interpretData = require('../app/interpretData');
+const parseData = require('../app/interpretData').parseData;
+const findObjWithKey = require('../app/interpretData').findObjectWithKey;
+const mergeObjects = require('../app/interpretData').mergeObjects;
 const fs = require('fs');
 const expect = require('chai').expect;
 
@@ -10,11 +12,55 @@ describe('Data Interpretation', function() {
 	describe('CSV Parsing', function() {
 		it ('correctly turns csv data into array of objects', function() {
 			const csvTest = fs.readFileSync("TestData1.csv", "utf8");
-			const output = interpretData(csvTest);
+			const output = parseData(csvTest);
 			console.log(output)
 			expect(output).to.be.an.instanceof(Array);
 			expect(output[0]).to.have.property('Name').that.is.a('string');
 			expect(output).to.have.lengthOf(5);
+		})
+	});
+	describe('FindObjByKey', function() {
+		it ('loops through an array to return any objects that contain that key and value', function() {
+
+			const array = [{
+				pig: 'oink',
+				cow: 'moo',
+				duck: 'quack'
+			}, {
+				pig: 1,
+				cow: 2,
+				duck: 3
+			},
+			{
+				pig: 'Peter',
+				cow: 'Cassie',
+				duck: 'Daryl'
+			}]
+			const key = 'pig';
+			const value = 1;
+			let found = findObjWithKey(array, key, value);
+
+			console.log(found);
+		})
+	})
+	describe('Merge objects', function() {
+		it ('merges obj2 into obj1 and returns new merged object', function() {
+			const duckAnimals = {
+				duck: 'quack',
+				fish: 'blurb',
+				plankton: 'cr',
+				goose: 'honk'
+			};
+
+			const farmAnimals = {
+				pig: 'oink',
+				cow: 'moo',
+				duck: 'quack'
+			};
+
+			let merged = mergeObjects(duckAnimals, farmAnimals);
+
+			expect(merged).to.have.property('cow', 'moo');
 		})
 	})
 })
